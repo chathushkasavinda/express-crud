15e8628cbf18f5d5c068950d8e6595b3c9dbbf00
chathushkasavinda <chathushkasavinda18@gmail.com>
2024-02-08 10:59:46 +0530

    content uploaded

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function User  () {
     const [user,setUser] = useState([{
@@ -8,6 +9,7 @@ function User  () {
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center,'>
         <div className='w-50 bg-white rounded p-3'>
+            <Link to="/create" className='btn btn-success'>Add +</Link>
             <table className='table'>
                 <thead>
                     <tr>
@@ -24,7 +26,10 @@ function User  () {
                                 <td>{user.Name}</td>
                                 <td>{user.Email}</td>
                                 <td>{user.Age}</td>
-                                <td><button>Edit</button><button>Delete</button></td>
+                                <td>
+                                <Link to="/update" className='btn btn-success'>Update</Link>
+                                    <button>Delete</button>
+                                </td>
                             </tr>
                         })
                     }
